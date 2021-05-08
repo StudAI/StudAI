7fc12b01814dcd806d6b564deda8341180160b8d
mollyxu <64995721+mollyxu@users.noreply.github.com>
2021-05-08 13:37:04 -0700

    completed landingpage

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import './styles.scss';
 
 ReactDOM.render(
   <Provider store={store}>
