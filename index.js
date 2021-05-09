372162f5b76262a2ae5408f4025b29851d8392ad
Sayan Saha <sayansaha1082@gmail.com>
2021-05-08 20:14:41 -0700

    remove unnecessary endpoint

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.use(express.json());
 const authRoute = require("./api/routes/auth");
 app.use("/api/users", authRoute);
 
-app.get("/", (req, res) => res.send("what's good"));
-
 app.listen(process.env.PORT || 8000);
