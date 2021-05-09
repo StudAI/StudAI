aa67d140683d39bd475ac314d70524e946f87504
Callum Fletcher <callumafletcher@gmail.com>
2021-05-09 08:30:36 -0700

    fix netlify

--- client/src/components/DownloadPage.js
+++ client/src/components/DownloadPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./DownloadPage.scss";
 function DownloadPage() {
   const history = useHistory();
