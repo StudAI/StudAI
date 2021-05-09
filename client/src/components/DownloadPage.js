5f9bfdd1c24a860c23de1908c81433744d91f0b8
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-09 09:14:37 -0400

    further revamp extension

--- client/src/components/DownloadPage.js
+++ client/src/components/DownloadPage.js
@@ -1,73 +1,85 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
-import './DownloadPage.scss';
+import "./DownloadPage.scss";
 function DownloadPage() {
   const history = useHistory();
   return (
-    <div className="download" style={{
-      overflow: "hidden"
-     
-    }}>
-      <div className="download_item"             style={{
-              paddingTop: "48vh",
-              overflow: "hidden"
-             
-            }}>
-      <Button
-            variant="contained"
-            className="landingpage_super_content_download"
+    <div
+      className="download"
+      style={{
+        overflow: "hidden",
+      }}
+    >
+      <div
+        className="download_item"
+        style={{
+          paddingTop: "48vh",
+          overflow: "hidden",
+        }}
+      >
+        <Button
+          variant="contained"
+          className="landingpage_super_content_download"
+          style={{
+            color: "black",
+            background: "#111B47",
+            textTransform: "capitalize",
+            marginLeft: "7vw",
+            marginRight: "9vw",
+            width: "175px",
+          }}
+        >
+          <a
             style={{
-              color: "black",
-              background: "#111B47",
-              textTransform: "capitalize",
-              marginLeft: "7vw",
-              marginRight: "9vw",
-              width:"175px",
+              color: "white",
+              textDecoration: "none",
             }}
-
+            href="../../../extension.zip"
+            download
           >
-            <a             style={{
-              color: "white",
-              textDecoration: "none"
-            }}
-                href="../../../extension.zip"
-                download
-              >
-                Download
-              </a>
-          </Button>
+            Download
+          </a>
+        </Button>
 
-      <Button
-            variant="contained"
-            className="landingpage_super_content_download"
+        <Button
+          variant="contained"
+          className="landingpage_super_content_download"
+          style={{
+            color: "white",
+            background: "#111B47",
+            textTransform: "capitalize",
+            marginLeft: "8vw",
+            marginRight: "7vw",
+            width: "175px",
+          }}
+          onClick={() => history.push("/download")}
+        >
+          <a
             style={{
               color: "white",
-              background: "#111B47",
-              textTransform: "capitalize",
-              marginLeft: "8vw",
-              marginRight: "7vw",
-              width:"175px"
+              textDecoration: "none",
             }}
-            onClick={() => history.push("/download")}
+            href="chrome://extensions/"
           >
             chrome://extensions/
-          </Button>
-      <Button
-            variant="contained"
-            className="landingpage_super_content_download"
-            style={{
-              color: "white",
-              background: "#111B47",
-              textTransform: "capitalize",
-              marginLeft: "7vw",
-              marginRight: "6vw",
-              width:"175x"
-            }}
-            onClick={() => history.push("/download")}
-          >
-            “Load Unpacked”
-          </Button>
+          </a>
+        </Button>
+        <Button
+          variant="contained"
+          className="landingpage_super_content_download"
+          style={{
+            color: "white",
+            background: "#111B47",
+            textTransform: "capitalize",
+            marginLeft: "7vw",
+            marginRight: "6vw",
+            width: "175x",
+          }}
+          onClick={() => history.push("/download")}
+        >
+          “Load Unpacked”
+        </Button>
       </div>
     </div>
   );
