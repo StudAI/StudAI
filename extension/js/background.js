49e73abf3722962bc90d23b4047b2b36d7269ce1
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-06-29 04:05:26 -0400

    Update background.js

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,4 +1,3 @@
-
 var storage = chrome.storage.local; // the storage where data is saved.
 var state, links, time, totalSeconds; // local vars that hold storage values
 
@@ -11,10 +10,8 @@ setInterval(checkSite, 500);
  * Description: It will update the active tab with the HTML page showing that
  * the page is blocked.
  */
-function blockSite() 
-{
-
-    chrome.tabs.update({url: "../html/blockedSite.html"})
+function blockSite() {
+  chrome.tabs.update({ url: "../html/blockedSite.html" });
 }
 
 /*
@@ -22,15 +19,20 @@ function blockSite()
  * Description: It will set the array to block major social media websites and
  * it will save it into the local storage.
  */
-function setDefault() 
-{
-    console.log("Set the default");
-    storage.set({"links": ['facebook.com','youtube.com','twitter.com', 
-        'linkedin.com', 'instagram.com']});
-    storage.set({"time": 0});
-    storage.set({"distractions": 0});
+function setDefault() {
+  console.log("Set the default");
+  storage.set({
+    links: [
+      "facebook.com",
+      "youtube.com",
+      "twitter.com",
+      "linkedin.com",
+      "instagram.com",
+    ],
+  });
+  storage.set({ time: 0 });
+  storage.set({ distractions: 0 });
 }
-
 
 /*
  * Function: setTime();
@@ -38,26 +40,21 @@ function setDefault()
  * the number of seconds by 1. And set the final number into the local
  * storage. This will be called every time a second is passed.
  */
-function setTime()
-{
+function setTime() {
+  chrome.storage.local.get(["state", "time"], function (data) {
+    // Gets data from the local storage
+    state = data.state;
+    totalSeconds = data.time;
 
-    chrome.storage.local.get(["state","time"], function(data)
-    {
-        // Gets data from the local storage
-        state = data.state;
-	    totalSeconds = data.time;
+    // if not active productive session, then reset the session.
+    if (!state) {
+      totalSeconds = 0;
+      return;
+    }
+    totalSeconds++;
 
-        // if not active productive session, then reset the session.
-        if(!state) 
-        {
-            totalSeconds = 0;
-            return;
-        }
-        totalSeconds++;
-
-        storage.set({"time" : totalSeconds});
-
-    });
+    storage.set({ time: totalSeconds });
+  });
 }
 
 /*
@@ -66,56 +63,34 @@ function setTime()
  * of blocked websites. If it is not part of the list, then it does nothing
  * and returns. If it is, then we redirect the user to the blockedSite.html.
  */
-function checkSite() 
-{
-    chrome.storage.local.get(["state","links", "distractions", "time"], function(data) 
-    {
-		function randomNum(){
-			Math.floor(Math.random() * 100) + 1
-		}
+function checkSite() {
+  chrome.storage.local.get(
+    ["state", "links", "distractions", "time"],
+    function (data) {
+      function randomNum() {
+        Math.floor(Math.random() * 100) + 1;
+      }
 
-        // Gets the data from the local storage
-        state = data.state;
-        links = data.links;
-		time = data.time;
-        distractions = data.distractions;
-        // If not active productive session, then continue as normal.
-        if(!state) return;
+      // Gets the data from the local storage
+      state = data.state;
+      links = data.links;
+      time = data.time;
+      distractions = data.distractions;
+      // If not active productive session, then continue as normal.
+      if (!state) return;
 
-        chrome.tabs.query({active:true, lastFocusedWindow: true}, tabs => 
-        {
+      chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
         if (tabs.length == 0) return;
         let url = tabs[0].url;
         if (url.includes("html/blockedSite.html")) return;
-            // checks every entry for a blocked URL.
-            for(index=0; index< links.length; index++) 
-            {
-
-                // check if there is a URL and if it should be blocked
-                if (url && url.includes(links[index])) 
-                {
-                    // This link shows when wanting to add a link to the blocked list
-                    if (url.includes("options.html?add_link=" + links[index])) return;
-
-                    // This will update the tab to not go to the blocked URL.
-					if (data.time>30){
-					var i = Math.floor(Math.random() * 150) + 30
-
-					if(distractions>i){
-						blockSite();
-						storage.set({"distractions" : 0})
-						storage.set({"time" : 0});
-					}
-				}
-					if (33>data.time>30){
-						storage.set({"distractions" : 0 })
-						storage.set({"time" : 0});
-					}
-                    storage.set({"distractions" : Math.ceil((distractions + Math.floor(Math.random() * 1.2)))})
-                        
-                    return;
-                }
-            }
-        });
-    })
+        // checks every entry for a blocked URL.
+        for (index = 0; index < links.length; index++) {
+          // check if there is a URL and if it should be blocked
+          if (url && url.includes(links[index])) {
+            blockSite();
+          }
+        }
+      });
+    }
+  );
 }
